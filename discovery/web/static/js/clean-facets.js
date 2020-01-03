591729d011b539a4ffa90fbdc9b32543c7abe26c
Laura Hughes <flaneuse@users.noreply.github.com>
2020-01-03 14:43:36 -0800

    change route on changed query

--- discovery/web/static/js/clean-facets.js
+++ discovery/web/static/js/clean-facets.js
@@ -16,7 +16,12 @@ cleanFacets = function(facets, facetSize = null) {
     summary['variable'] = varName.replace(".keyword", "");
     // summary['searchVariable'] = [varName];
     // basically a shim to convert term/count to key/value for consistency.
-    summary['counts'] = facets[varName]['terms'].map(d => { return {key: d.term, value: d.count}}).sort((a, b) => b.value - a.value);
+    summary['counts'] = facets[varName]['terms'].map(d => {
+      return {
+        key: d.term,
+        value: d.count
+      }
+    }).sort((a, b) => b.value - a.value);
     cleanedFacets.push(summary)
   })
 
@@ -47,22 +52,53 @@ combineFunders = function(facets, facetSize) {
 getQueryFilters = function(selectedFilters) {
   console.log(selectedFilters)
   let query_string = Object.keys(selectedFilters)
-  .filter(id => selectedFilters[id].length > 0)
-  .map(id => reduceQuery(id, selectedFilters))
-  .join(" AND ")
+    .filter(id => selectedFilters[id].length > 0)
+    .map(id => reduceQuery(id, selectedFilters))
+    .join(" AND ")
 
   console.log(query_string);
 
-  return(query_string)
+  return (query_string)
 }
 
 reduceQuery = function(id, selectedFilters) {
   let query_values = `("${selectedFilters[id].join('","')}")`;
   let query_string;
-  if(id === "funder") {
+  if (id === "funder") {
     query_string = `(funder.name.keyword:${query_values} OR funding.funder.name.keyword:${query_values})`;
   } else {
     query_string = `${id}.keyword:${query_values}`;
   }
-  return(query_string)
+  return (query_string)
+}
+
+filterString2Obj = function(filterStr) {
+  let filterObj = {
+    "funder": [],
+    "variableMeasured": [],
+    "measurementTechnique": [],
+    "keywords": []
+  };
+
+  if (filterStr) {
+    let filters = filterStr.split(" AND ");
+    console.log(filters)
+
+
+
+    filters.forEach(d => {
+      if (d.search("funder.name") > -1) {
+        let value = d.split(":").slice(-1)[0].replace("(", "[").replace("))", ")").replace(")", "]");
+        filterObj["funder"] = JSON.parse(value);
+      } else {
+        let filterComponents = d.replace(".keyword", "").split(":");
+        let key = filterComponents[0];
+        let value = JSON.parse(filterComponents[1].replace("(", "[").replace(")", "]"));
+        filterObj[key] = value;
+      }
+    })
+    console.log(filterObj)
+  }
+
+  return (filterObj)
 }
